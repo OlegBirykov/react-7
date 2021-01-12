41d32bbf858c8414fe53607eb4b0d99071d34f41
OlegBirykov <p_petro75@mail.ru>
2021-01-12 22:47:35 +0300

    Добавлена исходная версия кода заданий №1 и №2

---
+++ src/task2/VideoAndArticleApp.js
@@ -0,0 +1,99 @@
+import React, { useState } from 'react';
+import {nanoid} from 'nanoid';
+
+/*
+function New(props) {
+    return (
+        <div className="wrap-item wrap-item-new">
+            <span className="label">New!</span>
+            {props.children}
+        </div>
+    )
+};
+
+function Popular(props) {
+    return (
+        <div className="wrap-item wrap-item-popular">
+            <span className="label">Popular!</span>
+            {props.children}
+        </div>
+    )
+};
+*/
+function Article(props) {
+    return (
+        <div className="item item-article">
+            <h3><a href=".">{props.title}</a></h3>
+            <p className="views">Прочтений: {props.views}</p>
+        </div>
+    )
+};
+
+function Video(props) {
+    return (
+        <div className="item item-video">
+            <iframe src={props.url} title={props.url} frameBorder="0" allow="autoplay; encrypted-media" allowFullScreen></iframe>
+            <p className="views">Просмотров: {props.views}</p>
+        </div>
+    )
+};
+
+function List(props) {
+  const content = props.list.map(item => {
+    switch (item.type) {
+      case 'video':
+        return <Video {...item} key={nanoid()} />;
+
+      case 'article':
+        return <Article {...item} key={nanoid()} />;
+
+      default:
+        return null;
+    }
+  });
+
+  return (
+    <div className="item-list"> 
+      {content}
+    </div>   
+  );
+};
+
+export default function VideoAndArticleApp() {
+    const [list] = useState([
+        {
+            type: 'video',
+            url: 'https://www.youtube.com/embed/rN6nlNC9WQA?rel=0&amp;controls=0&amp;showinfo=0',
+            views: 50
+        },
+        {
+            type: 'video',
+            url: 'https://www.youtube.com/embed/dVkK36KOcqs?rel=0&amp;controls=0&amp;showinfo=0',
+            views: 12
+        },
+        {
+            type: 'article',
+            title: 'Невероятные события в неизвестном поселке...',
+            views: 175
+        },
+        {
+            type: 'article',
+            title: 'Секретные данные были раскрыты!',
+            views: 1532
+        },
+        {
+            type: 'video',
+            url: 'https://www.youtube.com/embed/TKmGU77INaM?rel=0&amp;controls=0&amp;showinfo=0',
+            views: 4253
+        },
+        {
+            type: 'article',
+            title: 'Кот Бегемот обладает невероятной...',
+            views: 12,
+        },
+    ]);
+
+    return (
+       <List list={list} />
+    );
+}
